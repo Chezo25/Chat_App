6992d96c17e913b2c318ba020c22921f567ed795
IslandBoyDev25 <chezling.stoddard@gmail.com>
2021-11-28 21:30:31 -0400

    buld out

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,8 +3,19 @@ use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
 
-
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
 
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind(LOCAL).expect("Listener to fail");
+    server.set_nonblocking(true).expect("failed to initialize block"):
+
+    let mut clients = vec![];
+    let (tx, rx) = mpsc::channel::<String>();
+    loop {
+        if let ok((mut socket, addr)) = server.accept() { //  this allows the connection to this server
+            println!("Client {} connected", addr );
+            let tx = tx.clone();
+        }
+    }
 }
